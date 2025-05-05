57fe92cc84fb76d8079f4a115bd76dca7f8a5eef
Morpheus <127767180+6Morpheus6@users.noreply.github.com>
2025-05-05 22:28:10 +0200

    nightly triton-windows for 50 series

--- torch.js
+++ torch.js
@@ -9,7 +9,7 @@ module.exports = {
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
           'uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128',
-          "uv pip install triton-windows",
+          "uv pip install -U --pre triton-windows",
           "uv pip install https://github.com/woct0rdho/SageAttention/releases/download/v2.1.1-windows/sageattention-2.1.1+cu128torch2.7.0-cp310-cp310-win_amd64.whl", 
 //          "uv pip install https://github.com/woct0rdho/triton-windows/releases/download/v3.2.0-windows.post9/triton-3.2.0-cp310-cp310-win_amd64.whl",
 //          "uv pip install https://github.com/deepbeepmeep/SageAttention/raw/refs/heads/main/releases/sageattention-2.1.0-cp310-cp310-win_amd64.whl"
